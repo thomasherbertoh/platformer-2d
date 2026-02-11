04a5488e5feb1fca6c56935f1db97ddac86036d0
thomasherbertoh <62878842+thomasherbertoh@users.noreply.github.com>
2026-02-11 19:58:38 +0100

    Closes 10 - squashes splashtimer and wintimer into one, generic timer (#14)

--- src/ui/resources.rs
+++ src/ui/resources.rs
@@ -1,6 +1,11 @@
 use bevy::{ecs::resource::Resource, time::Timer};
 
+use crate::game::states::{GameState, MenuState};
+
 #[derive(Resource)]
-pub struct SplashTimer(pub Timer);
-#[derive(Resource)]
-pub struct WinTimer(pub Timer);
+pub struct GameTimer {
+    pub timer: Timer,
+    pub target_state: GameState,
+    pub next_game_state: GameState,
+    pub next_menu_state: MenuState,
+}
